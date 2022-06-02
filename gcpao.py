24b49b359cac070a01ce76cb19f9904b1626eec8
ghostofpokemon <74148990+ghostofpokemon@users.noreply.github.com>
2022-06-02 16:04:55 -0500

    Update gcpao.py
    Added YearBuilt column and set min year to 2004

--- gcpao.py
+++ gcpao.py
@@ -3,14 +3,14 @@ import urllib.request
 from sys import argv
 
 
-url = f"https://www.bcpao.us/api/v1/export?usecodes=0110&zipcodes={argv[1]}&yblow=2012&activeonly=true&sortColumn=saleDate&sortOrder=desc&format=csv"
+url = f"https://www.bcpao.us/api/v1/export?usecodes=0110&zipcodes={argv[1]}&yblow=2004&activeonly=true&sortColumn=saleDate&sortOrder=desc&format=csv"
 with urllib.request.urlopen(url) as testfile, open(f"{argv[1]}" ".csv", "w") as f:
     f.write(testfile.read().decode())
 
 
 homes = pd.read_csv(f"{argv[1]}" ".csv", error_bad_lines=False)
 homes = homes.loc[homes["MailAddress"] == homes["SiteAddress"]]
-just = homes[["MailFormatted1", "MailAddress"]]
+just = homes[["MailFormatted1", "MailAddress", "YearBuilt"]]
 
 ## 32907
 # just[["Street", "City", "State", "Zip"]] = just.MailAddress.str.extract(
@@ -19,12 +19,13 @@ just = homes[["MailFormatted1", "MailAddress"]]
 
 ## 32940
 just[["Street", "City", "State", "Zip"]] = just.MailAddress.str.extract(
-    "(.+)(PALM BAY|MELBOURNE|PALM SHORES|ROCKLEDGE?)[ ]([a-zA-Z]{2})[ ]([0-9]{5})", expand=True
+    "(.+)(PALM BAY|MELBOURNE|PALM SHORES|ROCKLEDGE?)[ ]([a-zA-Z]{2})[ ]([0-9]{5})",
+    expand=True,
 )
 
 just[["Last", "First"]] = just.MailFormatted1.str.extract("(.+?)[,](.+)?", expand=True)
 
-final = just[["Last", "First", "Street", "City", "State", "Zip"]]
+final = just[["Last", "First", "Street", "City", "State", "Zip", "YearBuilt"]]
 final = final[final["Last"].notna()]
 print(final)
 final.to_excel(f"{argv[1]}" ".xlsx", index=False, header=True)
